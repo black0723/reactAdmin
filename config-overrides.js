48439b539303b22a57adb3b80174cecf8884a514
djj <1437764624@qq.com>
2019-07-11 00:05:21 +0800

    antd组件，路由，登录界面

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+//在antd的官网上有相关配置的介绍
+const {override, fixBabelImports, addLessLoader} = require('customize-cra');
+module.exports = override(
+  //实现按需打包
+  fixBabelImports('import',
+    {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true, //自动打包先关的样式
+    }
+  ),
+
+  //使用less-loader对源码中的less变量进行重新赋值
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: {'@primary-color': '#1DA57A'},
+  }),
+);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*根组件
+* 1.简单的：函数方式（无状态）
+* 2.复杂的：类定义（有状态）
+* */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+//引入路由组件
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+export default class App extends Component {
+
+  render() {
+    //必须要返回一个虚拟dom
+    return(
+      <BrowserRouter>
+        {/*只匹配其中一个*/}
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
-
-export default App;
